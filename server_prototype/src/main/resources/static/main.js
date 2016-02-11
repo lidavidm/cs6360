0771654779e919194fcfe0717b4ca159edc20530
David Li <li.davidm96@gmail.com>
2016-02-11 10:28:25 -0500

    Add some additional styling/structure

--- server_prototype/src/main/resources/static/main.js
+++ server_prototype/src/main/resources/static/main.js
@@ -28,9 +28,31 @@ var EditorComponent = {
 
     view: function(controller) {
         return m("div#editor", [
-            m("h1", "Editor"),
+            m("div#workspace", "Blocks go here"),
+            m("div#workbench", [
+                "Blueprints/toolboxes go here",
+                m.component(BlueprintComponent),
+            ]),
         ]);
     },
+};
+
+var BlueprintComponent = {
+    controller: function(args) {
+
+    },
+
+    view: function(controller) {
+        return m(".blueprint", [
+            m("header", [
+                m("span.class", "Robot"),
+                " can",
+            ]),
+            m(".methods", [
+                m(".method", "moveForward"),
+            ]),
+        ]);
+    }
 };
 
 var MapComponent = {
@@ -47,7 +69,7 @@ var MapComponent = {
                     align: "center",
                 };
 
-                game.add.text(game.world.centerX - 300, 0, text, style);
+                game.add.text(game.world.centerX, 0, text, style);
             },
         };
 
@@ -55,15 +77,19 @@ var MapComponent = {
     },
 
     view: function(controller) {
-        return m("div#map", {
-            config: function(element, isInitialized) {
-                if (!isInitialized) {
-                    controller.phaser = new Phaser.Game(426, 720, Phaser.CANVAS, element, {
-                        create: controller.create,
-                    });
-                }
-            },
-        });
+        return m("div#map", [
+            m("div#worldMap", {
+                config: function(element, isInitialized) {
+                    if (!isInitialized) {
+                        // TODO: figure out how to get this to scale properly
+                        controller.phaser = new Phaser.Game(384, 612, Phaser.CANVAS, element, {
+                            create: controller.create,
+                        });
+                    }
+                },
+            }),
+            m("nav#gameControls", "Controls"),
+        ]);
     },
 };
 
