8e074d648fb6789314a61eaa7a3ab4094e2a6485
David Li <li.davidm96@gmail.com>
2016-02-10 19:25:56 -0500

    Move child VM IO processing to threads

--- server_prototype/src/rakka/RakkaMain.java
+++ server_prototype/src/rakka/RakkaMain.java
@@ -1,7 +1,6 @@
 package rakka;
 
 import com.sun.jdi.Bootstrap;
-import com.sun.jdi.ThreadReference;
 import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.VirtualMachineManager;
 import com.sun.jdi.connect.Connector;
@@ -13,15 +12,35 @@ import com.sun.jdi.event.EventQueue;
 import com.sun.jdi.event.EventSet;
 import com.sun.jdi.event.VMDisconnectEvent;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Map;
 
 /**
  * Created by lidavidm on 2/10/16.
  */
 public class RakkaMain {
+    static class StreamHandlerThread extends Thread {
+        private final BufferedReader reader;
+        private final PrintStream writer;
+
+        public StreamHandlerThread(InputStream is, OutputStream os) {
+            reader = new BufferedReader(new InputStreamReader(is));
+            writer = new PrintStream(os);
+        }
+
+        @Override
+        public void run() {
+            try {
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    writer.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // For reference: http://stackoverflow.com/questions/21654255
         VirtualMachineManager vmm = Bootstrap.virtualMachineManager();
@@ -33,21 +52,23 @@ public class RakkaMain {
             VirtualMachine vm = connector.launch(arguments);
             vm.resume();
 
-//            EventQueue events = vm.eventQueue();
-//            EVENT_LOOP: while (true) {
-//                EventSet e = events.remove();
-//                for (Event event : e) {
-//                    System.out.println(event);
-//                    if (event instanceof VMDisconnectEvent) {
-//                        break EVENT_LOOP;
-//                    }
-//                }
-//            }
-            BufferedReader reader = new BufferedReader(new InputStreamReader(vm.process().getInputStream()));
-            reader.lines().forEach(System.out::println);
+            new StreamHandlerThread(vm.process().getInputStream(), System.out).run();
+            new StreamHandlerThread(vm.process().getErrorStream(), System.err).run();
+            EventQueue events = vm.eventQueue();
 
-            BufferedReader stderr = new BufferedReader(new InputStreamReader(vm.process().getErrorStream()));
-            stderr.lines().forEach(System.out::println);
+            EVENT_LOOP:
+            while (true) {
+                EventSet e = events.remove();
+                for (Event event : e) {
+                    if (event instanceof VMDisconnectEvent) {
+                        break EVENT_LOOP;
+                    }
+
+                    System.out.println(event);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (IllegalConnectorArgumentsException e) {
