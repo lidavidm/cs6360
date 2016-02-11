6d6fe828c08e954576fe07569bd34850319b123f
David Li <li.davidm96@gmail.com>
2016-02-11 10:54:07 -0500

    Show toolbox/blueprint structure

--- server_prototype/src/main/resources/static/main.js
+++ server_prototype/src/main/resources/static/main.js
@@ -31,6 +31,7 @@ var EditorComponent = {
             m("div#workspace", "Blocks go here"),
             m("div#workbench", [
                 "Blueprints/toolboxes go here",
+                m.component(ToolboxComponent),
                 m.component(BlueprintComponent),
             ]),
         ]);
@@ -43,17 +44,47 @@ var BlueprintComponent = {
     },
 
     view: function(controller) {
-        return m(".blueprint", [
+        return m(".blueprint.workbench-area", [
             m("header", [
                 m("span.class", "Robot"),
                 " can",
             ]),
             m(".methods", [
-                m(".method", "moveForward"),
+                m(".method.workbench-item", "moveForward"),
+                m(".method.workbench-item", "turnLeft"),
+                m(".method.workbench-item", "selfDestruct"),
+                m(".method.workbench-item", "reverse"),
             ]),
         ]);
     }
 };
+
+var ToolboxComponent = {
+    controller: function(args) {
+
+    },
+
+    view: function(controller) {
+        return m(".toolbox.workbench-area", [
+            m("header", "Toolbox"),
+            m(".control-flow-structures", [
+                m(".control-flow-structure.workbench-item", "if..else.."),
+                m(".control-flow-structure.workbench-item", "forever"),
+                m(".boolean.workbench-item", "true"),
+                m(".boolean.workbench-item", "false"),
+                m(".primitive.workbench-item", "text"),
+                m(".primitive.workbench-item", "number"),
+                m(".control-flow-structure.workbench-item", [
+                    "tell ",
+                    m("span.object", "object"),
+                    " to ",
+                    m("span.method", "do something"),
+                ]),
+            ]),
+        ]);
+    }
+};
+
 
 var MapComponent = {
     controller: function(args) {
