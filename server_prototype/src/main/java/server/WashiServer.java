9596ba4594d2d23fa42543ec618047560c1ec0cb
David Li <li.davidm96@gmail.com>
2016-02-10 22:20:53 -0500

    Add server/static files

---
+++ server_prototype/src/main/java/server/WashiServer.java
@@ -0,0 +1,16 @@
+package server;
+
+import static spark.Spark.*;
+
+/**
+ * Created by lidavidm on 2/10/16.
+ */
+public class WashiServer {
+    public static void main(String[] args) {
+        staticFileLocation("/static");
+
+        get("/", (request, response) -> {
+            return "Test";
+        });
+    }
+}
