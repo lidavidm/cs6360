94a9ab970cab1dc6b4053a1e6c5882905469e119
David Li <li.davidm96@gmail.com>
2016-02-22 17:30:19 -0500

    Switch to Google Blockly

---
+++ server_prototype/src/main/resources/static/custom_blocks.js
@@ -0,0 +1,66 @@
+// https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#y95qk2
+Blockly.Blocks["method"] = {
+    init: function() {
+        this.jsonInit({
+            "id": "method",
+            "message0": "%1",
+            "args0": [
+                {
+                    "type": "field_dropdown",
+                    "name": "method name",
+                    "options": [
+                        [
+                            "turn left",
+                            "turnLeft"
+                        ],
+                        [
+                            "move forward",
+                            "moveForward"
+                        ],
+                        [
+                            "self destruct",
+                            "selfDestruct"
+                        ]
+                    ]
+                }
+            ],
+            "inputsInline": true,
+            "output": null,
+            "colour": 260,
+            "tooltip": "",
+            "helpUrl": "http://www.example.com/"
+        });
+    },
+};
+
+// https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#85mctt
+Blockly.Blocks["tell"] = {
+    init: function() {
+        this.jsonInit({
+            "id": "tell",
+            "message0": "tell %1 %2 to %3 %4",
+            "args0": [
+                {
+                    "type": "input_dummy"
+                },
+                {
+                    "type": "input_value",
+                    "name": "object"
+                },
+                {
+                    "type": "input_dummy"
+                },
+                {
+                    "type": "input_value",
+                    "name": "method"
+                }
+            ],
+            "inputsInline": true,
+            "previousStatement": null,
+            "nextStatement": null,
+            "colour": 120,
+            "tooltip": "",
+            "helpUrl": "http://www.example.com/"
+        });
+    },
+}
