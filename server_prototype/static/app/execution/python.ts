ff4016ed4f6a09605f77238ba2698bd487497edc
David Li <li.davidm96@gmail.com>
2016-02-27 16:05:08 -0500

    feat(Python Interpreter): work around lack of __main__ for bdb

--- server_prototype/static/app/execution/python.ts
+++ server_prototype/static/app/execution/python.ts
@@ -11,6 +11,22 @@ export class Interpreter {
             totalMemory: 128 * 1024 * 1024,
             stdout: this._handle_stdout,
         });
+
+        this._vm.ready().then(() => {
+            this._vm.exec(`
+# Workaround for lack of __main__. See last commit in
+# https://github.com/pypyjs/pypyjs/issues/161
+import bdb
+import types
+
+scope = {'__name__': '__main__', '__package__': None}
+main = types.ModuleType('__main__')
+main.__dict__.update(scope)
+
+import sys
+sys.modules['__main__'] = main
+`);
+        });
     }
 
     step() {
