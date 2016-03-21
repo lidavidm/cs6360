01fb82ef9914f3eaed941652488ae5fc4b44fabc
David Li <li.davidm96@gmail.com>
2016-03-21 19:03:19 -0400

    WIP DO NOT MERGE

--- server_prototype/static/app/execution/session.ts
+++ server_prototype/static/app/execution/session.ts
@@ -1,16 +1,54 @@
 class Session {
     aborted: boolean = false;
     paused: boolean = false;
-    pauseContext: [() => void, () => void] = null;
+    pauseContext: () => void = null;
 
     constructor(interpreter, log, code, runDiff) {
         this.event = event;
         this.interpreter = interpreter;
         this.log = log;
         this.code = code;
-        this.promise = new Promise((resolveOuter, rejectOuter) => {
+        log.reset();
+        this.promise = new Promise(this.execute.bind(this));
+    }
 
+    private execute(resolveOuter, rejectOuter) {
+        this.modelWorld.log.reset();
+        this.interpreter.run(this.code).then(() => {
+            this.replay(resolveOuter);
+        }, (err: any) => {
+            // TODO: show the error to the user
+            console.log(err);
+            // TODO: if the error type is BlocklyError, highlight
+            // the block and add to it the error message
+            this.modelWorld.log.record(new model.Diff(model.DiffKind.Error, err.toString()));
+
+            this.replay(resolveOuter);
         });
+    }
+
+    private replay(resolveOuter) {
+        this.log.replay(this.runDiffWrapper.bind(this)).then(() => {
+            resolveOuter();
+        });
+    }
+
+    private runDiffWrapper(diff) {
+        return new Promise((resolve, reject) => {
+            if (this.aborted) {
+                reject();
+                this.aborted = false;
+                return;
+            }
+            else if (this.paused) {
+                this.pauseContext = () => {
+                    this.runDiff(resolve, reject);
+                };
+                return;
+            }
+
+            this.runDiff(resolve, reject);
+        })
     }
 
     abort() {
@@ -26,6 +64,10 @@ class Session {
     }
 
     step() {
+        this.pauseContext();
+    }
 
+    then(success, failure?) {
+        this.promise.then(success, failure);
     }
 }
