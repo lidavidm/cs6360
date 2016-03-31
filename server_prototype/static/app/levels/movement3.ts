ff007151f096d6d8025616e1034d60adefa1c1d6
Michael Mauer <mrm355@cornell.edu>
2016-03-31 00:32:57 -0400

    feat(Levels): rudimentary level 1

--- server_prototype/static/app/levels/movement3.ts
+++ server_prototype/static/app/levels/movement3.ts
@@ -4,42 +4,38 @@ import * as TooltipView from "views/tooltip";
 import * as python from "execution/python";
 import * as asset from "asset";
 
-export class MovementLevel1 extends BaseLevel {
+export class MovementLevel3 extends BaseLevel {
     public robot: model.Robot;
 
     initialize() {
         super.initialize();
 
-        this.toolbox = new Toolbox(true);
+        this.toolbox = new Toolbox();
         this.toolbox.addControl("tell");
-        let methods = this.toolbox.addClass("Robot", asset.Robot.Basic, model.Robot, [
+        this.toolbox.addClass("Robot", asset.Robot.Basic, model.Robot, [
             model.Robot.prototype.moveForward,
+            model.Robot.prototype.turnRight,
         ]);
-        let object = this.toolbox.addObject("robot", "Robot");
-
-        this.toolbox.addControl("tell", true, [], [
-            ["OBJECT", object.cloneNode(true)],
-            ["METHOD", methods[0].cloneNode(true)],
-        ]);
+        this.toolbox.addObject("robot", "Robot");
 
         this.objectives = [
             {
-                objective: `Move the robot [${asset.Robot.Basic}] forward`,
+                objective: `Move the robot [${asset.Robot.Basic}] around the corner`,
                 completed: false,
                 predicate: (level) => {
-                    return level.robot.getX() === 2 && level.robot.getY() === 2;
+                    return level.robot.getX() === 7 && level.robot.getY() === 3;
                 }
             },
         ];
 
         this.allTooltips = [
             [
-                new TooltipView.Tooltip(TooltipView.Region.Controls,
-                    "Load your code onto the robot and run it."),
+                // new TooltipView.Tooltip(TooltipView.Region.Controls,
+                //     "Load your code onto the robot and run it."),
                 new TooltipView.Tooltip(TooltipView.Region.Toolbox,
-                    "Just use the premade command for now!"),
-                new TooltipView.Tooltip(TooltipView.Region.Workspace,
-                    "Drag and drop commands here!"),
+                    "Check the robot's blueprint for a function to get around that corner."),
+                // new TooltipView.Tooltip(TooltipView.Region.Workspace,
+                //     "Right click and select duplicate to copy a command."),
             ],
         ];
 
@@ -69,7 +65,6 @@ export class MovementLevel1 extends BaseLevel {
         super.create();
 
         let map = this.game.add.tilemap("movement1");
-
         map.addTilesetImage("cave2", "tiles");
 
         let layer = map.createLayer(
@@ -81,7 +76,7 @@ export class MovementLevel1 extends BaseLevel {
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
         this.initWorld(map);
-        this.robot = new model.Robot("robot", 1, 1, model.Direction.EAST,
+        this.robot = new model.Robot("robot", 6, 2, model.Direction.EAST,
                                      this.modelWorld, this.foreground, "robot");
 
         this.modelWorld.log.recordInitEnd();
