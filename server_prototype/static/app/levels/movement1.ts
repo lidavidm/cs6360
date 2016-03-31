b0dab729a71f5de390bb409102b401c0864f80b4
Michael Mauer <mrm355@cornell.edu>
2016-03-31 16:45:02 -0400

    chore: more touchups on level 1

--- server_prototype/static/app/levels/movement1.ts
+++ server_prototype/static/app/levels/movement1.ts
@@ -14,10 +14,6 @@ export class MovementLevel1 extends BaseLevel {
         this.toolbox.addControl("tell");
         let methods = this.toolbox.addClass("Robot", asset.Robot.Basic, model.Robot, [
             model.Robot.prototype.moveForward,
-<<<<<<< HEAD
-            model.Robot.prototype.turnRight,
-=======
->>>>>>> f342466d8f5afc151813b03f706a3c6d235a8cd4
         ]);
         let object = this.toolbox.addObject("robot", "Robot");
 
@@ -31,11 +27,7 @@ export class MovementLevel1 extends BaseLevel {
                 objective: `Move the robot [${asset.Robot.Basic}] forward`,
                 completed: false,
                 predicate: (level) => {
-<<<<<<< HEAD
-                    return level.robot.getX() === 2 && level.robot.getY() === 3;
-=======
                     return level.robot.getX() === 2 && level.robot.getY() === 2;
->>>>>>> f342466d8f5afc151813b03f706a3c6d235a8cd4
                 }
             },
         ];
@@ -84,7 +76,7 @@ export class MovementLevel1 extends BaseLevel {
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
         this.initWorld(map);
-        this.robot = new model.Robot("robot", 1, 3, model.Direction.EAST,
+        this.robot = new model.Robot("robot", 1, 2, model.Direction.EAST,
                                      this.modelWorld, this.foreground, "robot");
 
         this.modelWorld.log.recordInitEnd();
