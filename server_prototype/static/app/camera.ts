9e5d0a955536fabab8f4a562cf35ba100e2bf438
David Li <li.davidm96@gmail.com>
2016-04-02 17:38:41 -0400

    fix(Map): allow camera to move all the way to the right
    Closes #53

--- server_prototype/static/app/camera.ts
+++ server_prototype/static/app/camera.ts
@@ -3,6 +3,7 @@
  * relevant objects to a group.
  */
 export class ZoomCamera {
+    game: Phaser.Game;
     public group: Phaser.Group;
     public position: Phaser.Point;
     public scale: Phaser.Point;
@@ -10,6 +11,7 @@ export class ZoomCamera {
     bounds: Phaser.Rectangle;
 
     constructor(game: Phaser.Game) {
+        this.game = game;
         this.group = game.add.group();
         this.position = new Phaser.Point(0, 0);
         this.scale = new Phaser.Point(1, 1);
@@ -23,9 +25,9 @@ export class ZoomCamera {
 
     update() {
         this.position.x = Math.max(this.position.x, 0);
-        this.position.x = Math.min(this.position.x, this.bounds.width);
+        this.position.x = Math.min(this.position.x, this.bounds.width + this.game.width);
         this.position.y = Math.max(this.position.y, 0);
-        this.position.y = Math.min(this.position.y, this.bounds.height);
+        this.position.y = Math.min(this.position.y, this.bounds.height + this.game.height);
 
         this.group.position.x = -this.position.x;
         this.group.position.y = -this.position.y;
