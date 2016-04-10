0f7c968d8100a9769ea17bf859f30da234f8fa3f
Michael Mauer <96mauer@gmail.com>
2016-04-10 19:36:42 -0400

    feat(Levels): initial hierarchy levels

---
+++ server_prototype/static/app/levels/hierarchy2.ts
@@ -0,0 +1,126 @@
+import * as model from "model/model";
+import {EditorContext, MAIN} from "model/editorcontext";
+import {BaseLevel, Toolbox} from "level";
+import * as TooltipView from "views/tooltip";
+import * as python from "execution/python";
+import * as asset from "asset";
+
+export class HierarchyLevel2 extends BaseLevel {
+    public robot: model.Robot;
+    public iron: model.Iron;
+
+    initialize() {
+        super.initialize();
+
+        this.missionTitle = "Reinforcements";
+        this.missionText = [
+            "Time to build another robot!",
+            "Use the 'new' block to CONSTRUCT an additional Robot.",
+        ];
+
+        this.toolbox = new Toolbox();
+        this.toolbox.addControl("tell");
+        this.toolbox.addControl("new");
+        this.toolbox.addClasses(["Robot"]);
+        this.toolbox.addClass("SmallRobot", asset.Robot.Basic, model.Robot, [
+            model.Robot.prototype.moveForward,
+            model.Robot.prototype.turnRight,
+            model.Robot.prototype.mine,
+        ]);
+        this.toolbox.addClass("Robot", asset.Robot.Basic, model.Robot, [
+            model.Robot.prototype.moveForward,
+            model.Robot.prototype.turnRight,
+            model.Robot.prototype.turnLeft,
+            model.Robot.prototype.mine,
+        ]);
+
+        this.toolbox.addObject("smallRobot", "SmallRobot");
+
+        this.toolbox.addControl("controls_repeat_ext");
+        this.toolbox.addNumber(0);
+
+        this.objectives = [
+            {
+                objective: `Build another Robot [${asset.Robot.Basic}]`,
+                completed: false,
+                predicate: (level) => {
+                    for (let object of this.modelWorld.getObjectByLoc(17, 4)){
+                        console.log(object.getName());
+                        if (object !== null && object.getName() !== "smallRobot"){
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            },
+        ];
+
+        this.allTooltips = [
+            [
+                new TooltipView.Tooltip(TooltipView.Region.Toolbox,
+                    "Use the blue prints object to create a new robot!"),
+                new TooltipView.Tooltip(TooltipView.Region.ButtonBar,
+                    "Get overviews of the blue prints in the object heirarchy."),
+            ],
+        ];
+
+        this.hierarchy = {
+            name: "object",
+            children: [
+                {
+                    name: "Robot",
+                    children: [
+                        {
+                            name: "SmallRobot",
+                            children: [],
+                            methods: ["moveForward", "turnRight", "mine"],
+                            userMethods: ["advance"],
+                        },
+                    ],
+                    methods: ["moveForward", "turnRight", "turnLeft", "mine"],
+                    userMethods: [],
+                },
+            ],
+        };
+    }
+
+    preload() {
+        super.preload();
+
+        this.game.load.image("tiles", "assets/tilesets/cave2.png");
+        this.game.load.tilemap("outside", "assets/maps/outside.json", null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image("robot", asset.Robot.Basic);
+        this.game.load.image("iron", asset.Iron.Basic);
+    }
+
+    create() {
+        // Create the world objects here.
+        super.create();
+
+        let map = this.game.add.tilemap("outside");
+        map.addTilesetImage("cave2", "tiles");
+
+        let layer = map.createLayer(
+            "Tile Layer 1", this.game.width, this.game.height, this.background);
+
+        let layer2 = map.createLayer(
+            "Tile Layer 2", this.game.width, this.game.height, this.background);
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+
+        this.initWorld(map);
+        this.robot = new model.Robot("smallRobot", 17, 3, model.Direction.NORTH,
+                                     this.modelWorld, this.foreground, "robot");
+
+        this.modelWorld.log.recordInitEnd();
+        this.program.instantiateGlobals(this.modelWorld, this.toolbox);
+    }
+
+    instantiateObject(className: string, varName: string): model.WorldObject {
+        if (!this.modelWorld.passable(17, 4)) {
+            return null;
+        }
+        return new model.Robot(varName, 17, 4, model.Direction.WEST,
+                               this.modelWorld, this.foreground, "robot");
+    }
+}
