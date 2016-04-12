ac5772b080c367fecbd26d148a9d4e418b6db2cc
David Li <li.davidm96@gmail.com>
2016-04-12 19:19:27 -0400

    feat(Content): set progression

--- server_prototype/static/app/progression.ts
+++ server_prototype/static/app/progression.ts
@@ -35,17 +35,29 @@ import {MovementLevel3} from "levels/movement3";
 import {MovementLevel4} from "levels/movement4";
 import {FuncDefsLevel1} from "levels/funcdefs1";
 import {FuncDefsLevel2} from "levels/funcdefs2";
+import {VacuumLevel1} from "levels/vacuum1";
+import {VacuumLevel2} from "levels/vacuum2";
+import {VacuumLevel3} from "levels/vacuum3";
+import {VacuumLevel4} from "levels/vacuum4";
+import {HierarchyLevel1} from "levels/hierarchy1";
+import {HierarchyLevel2} from "levels/hierarchy2";
+import {HierarchyLevel3} from "levels/hierarchy3";
 import {BasicsLevel1} from "levels/basic1";
 import {BasicsLevel2} from "levels/basic2";
 import {Alpha1Level} from "levels/alpha1";
 
 export const DEFAULT_PROGRESSION = new Progression([
-    // ["movement1", MovementLevel1],
-    // ["movement2", MovementLevel2],
-    // ["movement3", MovementLevel3],
-    // ["movement4", MovementLevel4],
+    ["movement1", MovementLevel1],
+    ["movement2", MovementLevel2],
+    ["movement3", MovementLevel3],
+    ["movement4", MovementLevel4],
+    ["funcdefs1", FuncDefsLevel1],
     ["funcdefs2", FuncDefsLevel2],
-    // ["basic1", BasicsLevel1],
-    // ["basic2", BasicsLevel2],
-    // ["alpha1", Alpha1Level],
+    ["vacuum1", VacuumLevel1],
+    ["vacuum2", VacuumLevel2],
+    ["vacuum3", VacuumLevel3],
+    ["vacuum4", VacuumLevel4],
+    ["hierarchy1", HierarchyLevel1],
+    ["hierarchy2", HierarchyLevel2],
+    ["hierarchy3", HierarchyLevel3],
 ]);
