95ffd04901623dea57456cdb26455e1d629e25ed
David Li <li.davidm96@gmail.com>
2016-04-19 10:55:25 -0400

    chore: add AGPL declaration to files in app

--- server_prototype/static/app/levels/funcdefs2.ts
+++ server_prototype/static/app/levels/funcdefs2.ts
@@ -1,3 +1,20 @@
+// Copyright 2016 David Li, Michael Mauer, Andy Jiang
+
+// This file is part of Tell Me to Survive.
+
+// Tell Me to Survive is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Tell Me to Survive is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with Tell Me to Survive.  If not, see <http://www.gnu.org/licenses/>.
+
 import * as model from "model/model";
 import {EditorContext, MAIN} from "model/editorcontext";
 import {BaseLevel, Toolbox} from "level";
